48b9d827d43742e519900bc1bbddd2d73998890a
FlowerSylveon <flufflysylv@gmail.com>
2025-12-11 14:48:19 -0300

    Testing installer

---
+++ Installers/Spot2yt-Installer.bat
@@ -0,0 +1,13 @@
+@echo off
+	title Installing Spot2yt.py
+	echo Downloading Spot2yt.py
+	echo.
+	curl -L https://github.com/masterofobzene/spot2yt/releases/download/1.0/spot2yt.zip > spot2yt.zip & tar -xf spot2yt.zip
+	if not exist "%userprofile%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe" do (curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" & python get-pip.py & del get-pip.py)
+	echo.
+	echo Installing requirements
+	echo.
+	python -m pip install -r requirements.txt
+	del requirements.txt&del spot2yt.zip
+	echo All done
+pause
