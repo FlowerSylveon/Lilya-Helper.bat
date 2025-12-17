a63e81c02740e860964ba8af3ec5c0f1fc5984f2
FlowerSylveon <flufflysylv@gmail.com>
2025-12-16 21:12:49 -0300

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,16 @@
-@echo off
+set installerpath="https://raw.githubusercontent.com/FlowerSylveon/Lilya-Helper.bat/refs/heads/Dev/Installers"
 
+echo What do you wish to install
 echo [Deno]    Decomplicates JS for YT-DLP downloading process
 echo [Spot2yt] Uses spotify links to download albums^|music^|artist
 echo [FFMPEG]  FFMPEG program
+
+set /p inst=">> "
+goto install-%inst%
+
+:install-deno
+	title Installing Deno & echo Installing Deno
+	curl -L %installerpath%/Deno.bat > Deno.bat
+	call Deno.bat
+	set message="Deno Installed"
+	del Deno.bat
